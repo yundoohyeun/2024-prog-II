3dda61cb61000199ed54c7a40c8f8aa2a9fe20bd
yundoohyeun <dbsengus11@naver.com>
2024-10-11 19:50:58 +0900

    prog 1~2 week

---
+++ 004_dornrrk/004_dornrrk.cpp
@@ -0,0 +1,12 @@
+﻿// 004_dornrrk.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("%d + %d = %d\n", 2,3,5);
+    printf("2 + 3 = 5");
+}
+
+
